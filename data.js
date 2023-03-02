c2e60aa87d07d8ba67f57fd0e21251be1c549c5f
beto081091 <beto_081091@hotmail.com>
2023-03-02 16:52:36 -0600

    Add files via upload

---
+++ data.js
@@ -0,0 +1,704 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-alberca-mirador",
+      "name": "Alberca mirador",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.33329823960091787,
+        "pitch": 0.049051364083247506,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.19560873920169008,
+          "pitch": 0.13184050457915397,
+          "rotation": 0,
+          "target": "1-jardn"
+        },
+        {
+          "yaw": -0.43544841811763035,
+          "pitch": 0.12581023356317544,
+          "rotation": 5.497787143782138,
+          "target": "2-alberca-superior"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.2141142180621749,
+          "pitch": 0.10137895877274872,
+          "title": "Canasta basquetbol",
+          "text": "Diviértete jugando en este espacio"
+        }
+      ]
+    },
+    {
+      "id": "1-jardn",
+      "name": "Jardín",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.22212166531873478,
+        "pitch": -0.00912662818577381,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.20737611925686927,
+          "pitch": -0.05805358150865558,
+          "rotation": 0,
+          "target": "0-alberca-mirador"
+        },
+        {
+          "yaw": 1.7138665007722373,
+          "pitch": -0.015656244508464923,
+          "rotation": 0,
+          "target": "2-alberca-superior"
+        },
+        {
+          "yaw": 3.1376488332302177,
+          "pitch": 0.04285861875161423,
+          "rotation": 0,
+          "target": "3-saln-de-eventos-1"
+        },
+        {
+          "yaw": -2.8891720260911935,
+          "pitch": 0.046252180287504885,
+          "rotation": 0.7853981633974483,
+          "target": "4-rea-de-juegos"
+        },
+        {
+          "yaw": 2.5304845369603415,
+          "pitch": 0.020101181211497732,
+          "rotation": 5.497787143782138,
+          "target": "7-cuarto-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-alberca-superior",
+      "name": "Alberca superior",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.2090762073413508,
+        "pitch": 0.006853606346375329,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.005735571966964059,
+          "pitch": 0.004601163294401189,
+          "rotation": 0,
+          "target": "1-jardn"
+        },
+        {
+          "yaw": -0.2450456928061442,
+          "pitch": -0.001772611113086242,
+          "rotation": 5.497787143782138,
+          "target": "3-saln-de-eventos-1"
+        },
+        {
+          "yaw": -1.8531613374851421,
+          "pitch": -0.09550312551066398,
+          "rotation": 0,
+          "target": "7-cuarto-1"
+        },
+        {
+          "yaw": -1.5565046800487305,
+          "pitch": -0.0934803593989777,
+          "rotation": 0.7853981633974483,
+          "target": "8-cuarto-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.3577922988221065,
+          "pitch": 0.003079656499735961,
+          "title": "Jacuzzi",
+          "text": "Contamos con jacuzzi con calefacción y masajeador de burbujas"
+        }
+      ]
+    },
+    {
+      "id": "3-saln-de-eventos-1",
+      "name": "Salón de eventos 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -3.047134560712241,
+        "pitch": -0.00711570863177613,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.3595090017071705,
+          "pitch": -0.08977934964167389,
+          "rotation": 0.7853981633974483,
+          "target": "4-rea-de-juegos"
+        },
+        {
+          "yaw": 0.025807341262455097,
+          "pitch": -0.0685749109000664,
+          "rotation": 0,
+          "target": "5-saln-de-eventos-2"
+        },
+        {
+          "yaw": 3.0381729465870038,
+          "pitch": 0.0810947375197113,
+          "rotation": 0,
+          "target": "1-jardn"
+        },
+        {
+          "yaw": -2.5641272711991228,
+          "pitch": 0.0872186932002279,
+          "rotation": 0.7853981633974483,
+          "target": "2-alberca-superior"
+        },
+        {
+          "yaw": -0.4201488639759674,
+          "pitch": -0.07586426882342145,
+          "rotation": 5.497787143782138,
+          "target": "7-cuarto-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.5739787889155634,
+          "pitch": -0.09797872436159238,
+          "title": "Salón de eventos",
+          "text": "Adecua este espacio de acuerdo a tus necesidades."
+        }
+      ]
+    },
+    {
+      "id": "4-rea-de-juegos",
+      "name": "Área de juegos",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 3.121945003611625,
+        "pitch": 0.0101521907765445,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.49889804216642375,
+          "pitch": -0.02324039722080329,
+          "rotation": 0.7853981633974483,
+          "target": "5-saln-de-eventos-2"
+        },
+        {
+          "yaw": -0.601641931387892,
+          "pitch": -0.005828400889964058,
+          "rotation": 5.497787143782138,
+          "target": "3-saln-de-eventos-1"
+        },
+        {
+          "yaw": 0.07055565532686181,
+          "pitch": -0.01480552074525221,
+          "rotation": 0,
+          "target": "7-cuarto-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-saln-de-eventos-2",
+      "name": "Salón de eventos 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -2.264398503507932,
+        "pitch": 0.10950400791941917,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.849207770649471,
+          "pitch": 0.00020120739286966227,
+          "rotation": 0,
+          "target": "3-saln-de-eventos-1"
+        },
+        {
+          "yaw": 3.124722876248118,
+          "pitch": -0.0064116374340539295,
+          "rotation": 4.71238898038469,
+          "target": "4-rea-de-juegos"
+        },
+        {
+          "yaw": -1.9510059515980398,
+          "pitch": 0.01555218270211256,
+          "rotation": 0,
+          "target": "7-cuarto-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.3591555011320082,
+          "pitch": 0.003997577669210273,
+          "title": "Salón de eventos",
+          "text": "Adecua este espacio de acuerdo a tus necesidades"
+        }
+      ]
+    },
+    {
+      "id": "6-mirador",
+      "name": "Mirador",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -3.1281670439590634,
+        "pitch": 0.02116923317953301,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-cuarto-1",
+      "name": "Cuarto 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 3.035585609999023,
+        "pitch": 0.1480683177886437,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6333721221093338,
+          "pitch": 0.04194026943572382,
+          "rotation": 0,
+          "target": "1-jardn"
+        },
+        {
+          "yaw": -0.355616850449028,
+          "pitch": 0.03297462357415348,
+          "rotation": 0.7853981633974483,
+          "target": "11-vista-cuarto-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.3287076255495087,
+          "pitch": 0.057848207629653814,
+          "title": "Cuarto para 2 personas",
+          "text": "Cuenta con cama king size, baño completo y espacio de closet"
+        }
+      ]
+    },
+    {
+      "id": "8-cuarto-2",
+      "name": "Cuarto 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.949340452408677,
+          "pitch": 0.001871518786145998,
+          "rotation": 0,
+          "target": "12-vista-cuarto-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.001655854623059838,
+          "pitch": 0,
+          "title": "Cuarto para 4 personas",
+          "text": "Cuenta con dos camas king size, baño completo y espacio de closet"
+        }
+      ]
+    },
+    {
+      "id": "9-cuarto-3",
+      "name": "Cuarto 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.025965059228346,
+          "pitch": 0.061387948526068215,
+          "rotation": 0,
+          "target": "13-vista-cuarto-3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0,
+          "pitch": 0,
+          "title": "Cuarto para 4 personas",
+          "text": "Cuenta con dos camas king size, baño completo y espacio de closet"
+        }
+      ]
+    },
+    {
+      "id": "10-cuarto-4",
+      "name": "Cuarto 4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 1.560840158957281,
+        "pitch": 0.012858035737409068,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4877657869264524,
+          "pitch": 0.02965625167552588,
+          "rotation": 0,
+          "target": "14-vista-cuarto-4"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.6729141755606101,
+          "pitch": 0.021586885412919088,
+          "title": "Cuarto para 2 personas",
+          "text": "Cuenta con cama king size, baño completo y espacio de closet"
+        }
+      ]
+    },
+    {
+      "id": "11-vista-cuarto-1",
+      "name": "Vista cuarto 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 2.872965513042999,
+        "pitch": 0.3830418657834542,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-vista-cuarto-2",
+      "name": "Vista cuarto 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 2.5327816215526333,
+        "pitch": 0.2541363310148519,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "13-vista-cuarto-3",
+      "name": "Vista cuarto 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -2.719174154208506,
+        "pitch": 0.10400798670715616,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "14-vista-cuarto-4",
+      "name": "Vista cuarto 4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 1.3918889223321322,
+        "pitch": 0.210418509440796,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Mirador Teques",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
